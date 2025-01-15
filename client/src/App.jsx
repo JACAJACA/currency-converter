33b3e670a7ed1215b25534fa10b0c37a3299ec27
jacek <jacekdusza33100@gmail.com>
2025-01-15 19:07:41 +0100

    exchangeRate-API connection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,6 @@ import Login from './Login'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
       <BrowserRouter>
